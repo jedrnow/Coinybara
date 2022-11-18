1e3691a5d7cde3e38ae9ce4ff28608c1b89d3f52
jinxxur <jnvqwerty@gmail.com>
2022-11-18 20:01:44 +0100

    Finished mining feature.

---
+++ src/block.ts
@@ -0,0 +1,28 @@
+import { TransactionData } from "./transactions";
+
+export interface BlockData{
+    index: number;
+    hash: string;
+    previousHash: string;
+    nonce: number;
+    transactions: TransactionData[];
+    key: string;
+}
+
+export default class Block implements BlockData{
+    constructor(
+        public index: number = 0,
+        public hash: string = '',
+        public previousHash: string = '',
+        public nonce: number = 0,
+        public transactions: TransactionData[] = [],
+    ){}
+    
+    get key(): string {
+        return JSON.stringify(this.transactions) + this.index + this.previousHash + this.nonce;
+    }
+
+    public addTransaction(transaction: TransactionData): void {
+        this.transactions = [...this.transactions, transaction];
+    }
+}
---
+++ src/blockchain.ts
@@ -0,0 +1,58 @@
+import sha256 from 'js-sha256';
+import Block, { BlockData } from "./block";
+import { TransactionData } from "./transactions";
+
+export interface BlockChainData{
+    blocks: BlockData[];
+    addBlock(block: BlockData): void;
+    getNextBlock(transactions: TransactionData[]): BlockData;
+    getPreviousBlock(): BlockData;
+    generateHash(block: BlockData): string;
+}
+
+export default class Blockchain implements BlockChainData{
+    public blocks: BlockData[];
+
+    constructor(genesisBlock: BlockData){
+        this.blocks = [];
+        this.addBlock(genesisBlock);
+    }
+
+    public addBlock(block: BlockData): void {
+        if(this.blocks.length === 0){
+            block.previousHash = '0000000000';
+            block.hash = this.generateHash(block);
+        }
+        this.blocks = [...this.blocks, block];
+    }
+
+    public getNextBlock(transactions: TransactionData[]): BlockData {
+        let block = new Block();
+
+        transactions.map(function(t: TransactionData){
+            block.addTransaction(t);
+        });
+
+        let previousBlock = this.getPreviousBlock();
+        block.index = this.blocks.length;
+        block.previousHash = previousBlock.hash;
+        block.hash = this.generateHash(block);
+        return block;
+    }
+
+    public getPreviousBlock(): BlockData {
+        return this.blocks[this.blocks.length - 1];
+    }
+
+    public generateHash(block: BlockData): string {
+        let hash = sha256(block.key);
+
+        // mining
+        while(!hash.startsWith('083a')){
+            block.nonce += 1;
+            hash = sha256(block.key);
+            console.log(hash);
+        }
+        return hash;
+    }
+}
